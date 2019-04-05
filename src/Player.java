4cef2aaaf5e8cc072fdccef31221d8ea1152da4b
Ryan Goldberg <ryan.s.goldberg@gmail.com>
2019-04-05 17:12:20 -0400

    First draft of Player.java, as new randomNumberGenerator in Utilities.java

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 /**
- * PURPOSE OF CLASS
  * @author Goldberg
  */
 
@@ -17,34 +16,91 @@ public class Player {
 
     private ArrayList<Property> inventory;
 
-
-    public static void move(){
-        //TODO
+    public Player(String name) {
+        this.name = name;
+        this.wallet = 1500;
+        this.position = 0;
+        this.numberOfJailCards = 0;
+        this.inJail = false;
+        this.turnsLeftInJail = 0;
+        this.inventory = new ArrayList<>();
     }
 
-    public int removeMoney(int amount){
-        //TODO
-        //What to do if wallet is empty/ more money is needed than is held in wallet
-        return 0;
+    public void move(int numberOfSpaces){
+        position += numberOfSpaces;
+        if(position > 39){
+            position = position%40;
+            passGo();
+        }
     }
 
-    public int addMoney(int amount){
-        //TODO
-        return 0;
+    public void moveTo(int newPos){
+        if(newPos < position){
+            move((39-position)+newPos);
+            passGo();
+        }else {
+            move(newPos-position);
+        }
+    }
+
+    public void removeMoney(int amount){
+        if(wallet - amount > 0){
+            wallet -= amount;
+        }else {
+            int extra = amount - wallet;
+            wallet = 0;
+            mortgageMode(extra);
+        }
+    }
+
+    public void addMoney(int amount){
+        wallet+=amount;
     }
 
     public void passGo(){
-        //TODO
+        wallet+=200;
     }
 
     public void sendToJail(){
+        turnsLeftInJail = 3;
+        inJail = true;
+        moveTo(10);
+    }
+
+    public void drawCard(){
+        int random = Utilities.generateNumber(1,101);
+
+        if(random < 20){//Get money
+            random = Utilities.generateNumber(1,10);
+            addMoney(20*random);
+        }else if(random < 50){//Give money
+            random = Utilities.generateNumber(1,10);
+            removeMoney(15*random);
+        }else if(random < 99){//Move spaces
+            while (random != 0){
+                random = Utilities.generateNumber(-3,3);
+            }
+            move(random);
+        }else {//Get out of jail card
+            numberOfJailCards++;
+        }
+    }
+
+    public void mortgageMode(int debt){
         //TODO
+        //Player cannot leave this mode until debt is paid, if they have no more properties then game is over for player
     }
 
-    public static void drawCard(){
-        //1,2,3,4
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", wallet=" + wallet +
+                ", position=" + position +
+                ", numberOfJailCards=" + numberOfJailCards +
+                ", inJail=" + inJail +
+                ", turnsLeftInJail=" + turnsLeftInJail +
+                ", inventory=" + inventory +
+                '}';
     }
-
-
-
 }
--- src/Utilities.java
+++ src/Utilities.java
@@ -8,7 +8,12 @@ public class Utilities {
         return (int)(Math.random()*6+1);
     }
 
-    public static int generateNumber(int end){
-        return (int)(Math.random()*end+1);
+    public static int generateNumber(int start, int end){//Inclusive-Exclusive
+        if(end < start){
+            System.err.println("Invalid Input: Utilities.java 'generateNumber'");
+            System.exit(1);
+        }
+
+        return start+(int)(Math.random()*(end-start));
     }
 }
