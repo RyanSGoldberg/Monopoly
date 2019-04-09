53aec22754ae9620bdc8b605e6c82c5a781968fc
bennyzaionz <bennyzaionz@yahoo.ca>
2019-04-09 12:08:04 -0400

    Buy Method

--- src/Tile.java
+++ src/Tile.java
@@ -16,4 +16,36 @@ public class Tile{
 
     public void landedOn(Player p){
     }
+
+    public Utilities.Type getType() {
+        return type;
+    }
+
+    public void setType(Utilities.Type type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getLocation() {
+        return location;
+    }
+
+    public void setLocation(byte location) {
+        this.location = location;
+    }
+
+    public byte getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(byte groupName) {
+        this.groupName = groupName;
+    }
 }
