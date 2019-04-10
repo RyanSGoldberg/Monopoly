5faba4e249ff6df110bbd8c448d7dcb7c7103d7b
bennyzaionz <bennyzaionz@yahoo.ca>
2019-04-10 15:34:51 -0400

    Deleted some stuff I did bad in tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -15,36 +15,4 @@ public class Tile{
 
     public void landedOn(Player p){
     }
-
-    public Utilities.Type getType() {
-        return type;
-    }
-
-    public void setType(Utilities.Type type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLocation() {
-        return location;
-    }
-
-    public void setLocation(byte location) {
-        this.location = location;
-    }
-
-    public int getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(byte groupName) {
-        this.groupName = groupName;
-    }
 }
