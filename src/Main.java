f5b6cb41bf8b48d476967ed5a9700c6863e02f27
Ryan Goldberg <ryan.s.goldberg@gmail.com>
2019-04-30 12:40:02 -0400

    SDK fix

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
 
-        Board.saveBoard();
+        //Board.saveBoard();
 
         Application.launch(Display.class,args);
     }
